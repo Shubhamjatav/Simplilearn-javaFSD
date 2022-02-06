2286e2661c4ee8d2600dc9adb05054fb4b99cf6c
Shubhamjatav <98072549+Shubhamjatav@users.noreply.github.com>
2022-02-06 15:59:08 +0530

    uncheckedexception

--- Demo2.java
+++ Demo2.java
@@ -1,11 +1,19 @@
-package com.simplilearn.checkedexception;
+package com.simplilearn.uncheckedexception;
 
-import java.io.FileReader;
+import java.util.Scanner;
 
 public class Demo2 {
-
+	
 	public static void main(String[] args) {
 		
-		FileReader file= new FileReader("C://test//user.text");
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter any number");
+		String num1 = sc.next();
+		
+		//lets convert string to number
+		int number=Integer.parseInt(num1);
+		
+		System.out.println("Number is "+number);
+		System.out.println("End of the program");
 	}
-}
+	}
