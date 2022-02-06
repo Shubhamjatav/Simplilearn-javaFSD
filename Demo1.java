8c37feb0d0df08c2d25520983e116987b95f8da0
Shubhamjatav <98072549+Shubhamjatav@users.noreply.github.com>
2022-02-06 15:53:01 +0530

    checkedexception

---
+++ Demo1.java
@@ -0,0 +1,9 @@
+package com.simplilearn.checkedexception;
+
+public class Demo1 {
+	
+	public static void main(String[] args) {
+		//complie time:Interruptedexception
+		Thread.sleep(1000);
+	}
+}
---
+++ Demo2.java
@@ -0,0 +1,11 @@
+package com.simplilearn.checkedexception;
+
+import java.io.FileReader;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		FileReader file= new FileReader("C://test//user.text");
+	}
+}
