05e8a9e701d94e9e52e11de0b934af92de1a284c
Shubhamjatav <98072549+Shubhamjatav@users.noreply.github.com>
2022-02-06 15:55:36 +0530

    MultiThreading

---
+++ ThreadDemo1.java
@@ -0,0 +1,27 @@
+package com.simplilearn.multithreading;
+
+public class ThreadDemo1 extends Thread {
+
+   public void run()
+   {
+	   for(int i=1;i<=5;i++)
+	   {
+		   System.out.println(i+ " "+Thread.currentThread().getName());
+		   try {
+			   Thread.sleep(1000);
+		   }catch (Exception e) {
+			   System.out.println(e);
+		   }
+	   }
+   }
+   
+   public static void main(String[] args) {
+	   ThreadDemo1 t1=new ThreadDemo1();
+	   ThreadDemo1 t2=new ThreadDemo1();
+	   
+	   t1.setName("first");
+	   t2.setName("second");
+	   t1.start();
+	   t2.start();
+   }
+}
